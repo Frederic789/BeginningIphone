f15cf1c792084ada3eab9f9b6fae804c5f30cb5c
Freddy <fredericdouamba2014@gmail.com>
2023-10-09 16:54:13 -0700

    class activity chap18

---
+++ BeginingIphone/ImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageView.swift
+//  BeginingIphone
+//
+//  Created by Student Account on 10/9/23.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageView()
+}
---
+++ BeginingIphone/LazyvView.swift
@@ -0,0 +1,18 @@
+//
+//  LazyvView.swift
+//  BeginingIphone
+//
+//  Created by Student Account on 10/9/23.
+//
+
+import SwiftUI
+
+struct LazyvView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyvView()
+}
