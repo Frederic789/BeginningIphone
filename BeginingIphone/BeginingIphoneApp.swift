3144d1dc766396b9101604aee9a9619d59aba81c
Freddy <fredericdouamba2014@gmail.com>
2023-10-09 16:06:54 -0700

    Initial Commit

---
+++ BeginingIphone/BeginingIphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeginingIphoneApp.swift
+//  BeginingIphone
+//
+//  Created by Student Account on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BeginingIphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
