8ca0325c2d71ff7f0e5ab5bcd0e3578cbc7473ec
Freddy <fredericdouamba2014@gmail.com>
2023-10-09 17:36:46 -0700

    update images

---
+++ BeginingIphone/UploadImage.swift
@@ -0,0 +1,18 @@
+//
+//  UploadImage.swift
+//  BeginingIphone
+//
+//  Created by Student Account on 10/9/23.
+//
+
+import SwiftUI
+
+struct UploadImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UploadImage()
+}
